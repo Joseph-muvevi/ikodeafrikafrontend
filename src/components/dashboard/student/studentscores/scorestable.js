2f0fa64baf21df46107cb0c09299dc2c12a337b6
SamsAgency <samstore637@gmail.com>
2021-10-18 16:46:50 +0300

    added score table

---
+++ src/components/dashboard/student/studentscores/scorestable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./studentscores.css"
+
+const ScoresTable = () => {
+	return (
+		<div className="student-scores-table">
+			table here
+		</div>
+	)
+}
+
+export default ScoresTable
--- src/components/dashboard/student/studentscores/studentscores.js
+++ src/components/dashboard/student/studentscores/studentscores.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import ScoresTable from './scorestable'
 import "./studentscores.css"
 
 const MyScores = () => {
 	return (
-		<div className="my-scores">
-			<h3>
-				I am my scores
-			</h3>
+		<div className="student-courses">
+			<div className="student-courses-content">
+				<h3 className="student-courses-h3">
+					My courses
+				</h3>
+				<div className="student-courses-p-and-filters">
+					<p classNAme="student-courses-p">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+					</p>
+				</div>
+				<ScoresTable/>
+			</div>
 		</div>
 	)
 }
